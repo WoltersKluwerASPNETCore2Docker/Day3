2f36515201feed0efb987bba4e1d390f97feb9c2
Jeff McBride <jeff@geeklearn.com>
2018-02-27 23:09:14 -0600

    Added Day 3 Course Files.

---
+++ Module 13 - Angular and Web APIs/Complete/EComm/EComm.WebAPI/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EComm.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EComm.WebAPI
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddDbContext<DataContext>(options => 
+                    options.UseSqlServer(Configuration.GetConnectionString("ECommConnection")))
+                .AddMvc()
+                .AddXmlDataContractSerializerFormatters()
+                .AddMvcOptions(options => 
+                    options.FormatterMappings.SetMediaTypeMappingForFormat("xml", "application/xml"));
+                //.AddMvcCore()
+                //.AddJsonFormatters();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            DisplayConfiguration();
+        }
+
+        private void DisplayConfiguration()
+        {
+            Console.WriteLine("******************************************");
+            Console.WriteLine("Configuration Example");
+            Console.WriteLine("******************************************");
+
+            Console.WriteLine("*********** appsettings.json *************");
+            Console.WriteLine($"ConnectionStrings:DefaultConnection " +
+                $"{(Configuration["ConnectionStrings:DefaultConnection"])}");
+        }
+    }
+}
---
+++ Module 16 - Logging/Begin/EComm/EComm.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EComm.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EComm.WebAPI.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/Product")]
+    public class ProductController : Controller
+    {
+        private DataContext ECommDataContext { get; }
+        public ProductController(DataContext dataContext)
+        {
+            ECommDataContext = dataContext;
+        }
+
+        // GET: api/Product
+        //[HttpGet]
+        //public IEnumerable<Product> Get()
+        //{
+        //    return ECommDataContext.Products.ToList();
+        //}
+        // Lambda Style
+        [HttpGet]
+        public IEnumerable<Product> Get() => ECommDataContext.Products.ToList();
+
+        // GET: api/Product/5
+        [HttpGet("{id}/{format?}", Name = "Get")]
+        [FormatFilter]
+        [Produces("application/json", "application/xml")]
+        public IActionResult Get(int id)
+        {
+            var product = ECommDataContext.Products
+                //.Include(p => p.Supplier)
+                .SingleOrDefault(p => p.Id == id);
+            if (product == null) return NotFound();
+
+            // Helper Method
+            //return Ok(product);
+
+            // ObjectResult base class for Helper Methods
+            return new ObjectResult(product);
+        }
+        
+        // POST: api/Product
+        [HttpPost]
+        public IActionResult Post([FromBody]Product product)
+        {
+            if (product == null) return BadRequest();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (ProductExists(product.Id)) return StatusCode(StatusCodes.Status409Conflict);
+
+            try
+            {
+                ECommDataContext.Products.Add(product);
+                ECommDataContext.SaveChanges();
+            }
+            catch (DbUpdateException) { return StatusCode(StatusCodes.Status409Conflict); }
+            catch (Exception) { return StatusCode(StatusCodes.Status500InternalServerError); }
+
+            return CreatedAtAction("Get", new { id = product.Id }, product);
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Product product)
+        {
+            if (product == null || id != product.Id) return BadRequest();
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            if (!ProductExists(id)) return NotFound();
+
+            var existing = ECommDataContext.Products.SingleOrDefault(p => p.Id == id);
+
+            try
+            {
+                existing.ProductName = product.ProductName;
+                existing.UnitPrice = product.UnitPrice;
+                existing.Package = product.Package;
+                existing.IsDiscontinued = product.IsDiscontinued;
+                existing.SupplierId = product.SupplierId;
+                ECommDataContext.SaveChanges();
+            }
+            catch (DbUpdateException) { return StatusCode(StatusCodes.Status409Conflict); }
+            catch (Exception) { return StatusCode(StatusCodes.Status500InternalServerError); }
+
+            return Ok(existing);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (!ProductExists(id)) return NotFound();
+            var existing = ECommDataContext.Products.SingleOrDefault(p => p.Id == id);
+
+            try
+            {
+                ECommDataContext.Remove(existing);
+                ECommDataContext.SaveChanges();
+            }
+            catch (DbUpdateException) { return StatusCode(StatusCodes.Status409Conflict); }
+            catch (Exception) { return StatusCode(StatusCodes.Status500InternalServerError); }
+
+            return Ok(existing);
+        }
+
+        private bool ProductExists(int id) => ECommDataContext.Products.Count(p => p.Id == id) > 0;
+    }
+}
